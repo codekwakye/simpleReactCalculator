e5c82838ddec247f5db41c9844d29eac66786f0a
codekwakye <kwakyejosephata@gmail.com>
2024-02-21 03:20:09 +0000

    my first calculator in react

---
+++ jj.jsx
@@ -0,0 +1,27 @@
+
+
+//const hello = React.createElement("h1",{}, "hello React");
+//ReactDOM.render(hello, document.getElementById("root"));
+
+//function Hello (){
+   // return <h1>hello React Hi </h1>;
+//}
+
+//ReactDOM.render(<Hello/>, document.getElementById("root"));
+
+//cont Hello = () => (
+   // <div>
+    //    <h1>hello react</h1>
+    //</div>
+//)
+
+//ReactDOM.render(<Hello/>, document.getElementById("root"));
+
+// properties in react or  props in react 
+
+//const Hello = (props) => (
+   // <div>
+      //  <h1>hello react hi {prros.name} you are {props.age} years old </h1>
+    //</div>
+//)
+//ReactDOM.render(<Hello name='bob' age={19}/>, document.getElementById("root"));
